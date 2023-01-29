ef390bd507ae4ca3b5d9bdd8ccda2f44ac866082
Subham Gupta <99577900+0subham1@users.noreply.github.com>
2023-01-29 21:39:27 +0530

    vercel json added

--- db/config.js
+++ db/config.js
@@ -4,7 +4,6 @@ require("dotenv").config()
 // const DB_URL =process.env.DB_URL
 const DB_URL="mongodb+srv://eCommerce:eCommerce@cluster0.3wz3tca.mongodb.net/e_comm?retryWrites=true&w=majority";
 
-console.log("DB_URL",DB_URL)
 
 mongoose.connect(DB_URL,()=>{
   console.log("db connected")
