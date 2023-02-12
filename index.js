68d2d213a12b2f1de269c653b12dcf9d6454b385
Subham Gupta <99577900+0subham1@users.noreply.github.com>
2023-02-12 08:37:18 +0530

    back12feb2

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ app.use(express.json()); //middleware
 
 const cors = require("cors");
 app.use(cors());
-
+ 
 
 //API IMPORTS
 app.use("",require("./API/usersApi"))
 app.use("",require("./API/itemsApi"))
 app.use("",require("./API/ordersApi"))
+
+const users = require("./db/usersModel");
+
+app.get("/users1", async (req, res) => {
+  let result = await users.find();
+  res.send(result);
+});
 
 app.get("/",  (req, res) => {
   res.send("hello world");
